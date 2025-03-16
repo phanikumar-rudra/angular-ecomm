6a827c20ae0ba09bd27c299b809770606abe6018
phani kumar <phanikumar2023@gmail.com>
2025-03-16 00:34:59 -0400

    order summary functionality

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CartItemComponent } from "./cart-item/cart-item.component";
+import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, OrderSummaryComponent],
   template: `
     <div class="p-6 flex flex-col gap-4">
       <h1 class="text-2xl ">Shopping cart</h1>
@@ -13,9 +14,8 @@ import { CartItemComponent } from "./cart-item/cart-item.component";
       @for (item of cartService.cart(); track item.id) {
         <app-cart-item [item]="item"/>
       }
+      <app-order-summary/>
     </div>
-
-
   `,
   styles: ``
 })
